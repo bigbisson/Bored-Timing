63d4836d6120fb74d0e3d4b3bc7f40c841065e61
Indra Nurwibisono <11659118+bigbisson@users.noreply.github.com>
2020-06-14 20:26:59 +0700

    Project 2 file py

---
+++ DTS-CiscoPython-2020/ProjectTugas/p2.py
@@ -0,0 +1,81 @@
+# nama file p2.py
+# Isikan email anda dan copy semua cell code yang dengan komentar #Graded
+
+# untuk revisi dan resubmisi sebelum deadline
+# silakan di resubmit dengan nilai variable priority yang lebih besar dari
+# nilai priority submisi sebelumnya
+# JIKA TIDAK ADA VARIABLE priority DIANGGAP priority=0
+import random
+priority = 0
+
+# netacad email cth: 'abcd@gmail.com'
+email = 'bigmasterindra@yahoo.com'
+
+# copy-paste semua #Graded cells YANG SUDAH ANDA KERJAKAN di bawah ini
+# Graded
+
+
+def isPointInCircle(x, y, r, center=(0, 0)):
+    # MULAI KODEMU DI SINI
+    pass
+    Boolean = None
+    if (((x - center[0])**2) + ((y - center[1])**2)) <= r**2:
+        Boolean = True
+    else:
+        Boolean = False
+    return Boolean
+
+
+# Graded
+
+
+def generateRandomSquarePoints(n, length, center=(0, 0)):
+    # MULAI KODEMU DI SINI
+    minx = center[0]-length/2
+    miny = center[1]-length/2
+
+    # Gunakan list comprehension dengan variable minx, miny, length, dan n
+    points = [[random.uniform(minx, length/2),
+               random.uniform(miny, length/2)] for i in range(n)]
+
+    return points
+
+# Graded
+
+
+def MCCircleArea(r, n=100, center=(0, 0)):
+    # MULAI KODEMU DI SINI
+    pass
+    titik = 0
+    points = generateRandomSquarePoints(n, r*2)
+
+    for i in points:
+        if isPointInCircle(i[0], i[1], r) == True:
+            titik += 1
+    luasLingkaran = (titik / n) * r*2**2
+
+    return luasLingkaran
+
+# Graded
+
+
+def LLNPiMC(nsim, nsample):
+    # MULAI KODEMU DI SINI
+    pass
+    totalSim = 0
+    for i in range(nsim):
+        totalSim += MCCircleArea(1, nsample)
+    rataSim = totalSim / nsim
+
+    return rataSim
+
+# Graded
+
+
+def relativeError(act, est):
+
+    # MULAI KODEMU DI SINI
+    pass
+    e = abs((est - act) / act) * 100
+
+    return e
