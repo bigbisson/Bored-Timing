669170e7e25167353e726b092fc17e1a9c07495c
Indra Nurwibisono <11659118+bigbisson@users.noreply.github.com>
2020-07-12 08:30:40 +0700

    Revisi SBA2020

--- DTS-CiscoPython-2020/SBA/SBA2020.py
+++ DTS-CiscoPython-2020/SBA/SBA2020.py
@@ -11,10 +11,10 @@ email = 'bigmasterindra@yahoo.com'
 # >>>>>>Soal 1
 
 
-def fungsiIO():
+def fungsiIO(x):
     pass
     # Mulai kode anda dari sini
-    nPut = list(map(int, input().split()))
+    nPut = x
     for i in (sorted(nPut)):
         print('*'*i)
 # >>>>>>Akhir Soal 1
@@ -63,31 +63,35 @@ class MoneyChanger:
         pass
         # Mulai kode anda dari sini
         self.base = base
-        self.dcurrency = dcur2idr
+        self.dcurrency = dcurrency
         self.percent = percent
+        for i in self.dcurrency:
+            if self.dcurrency[i] != self.dcurrency[self.base]:
+                self.dcurrency[i] = self.dcurrency[i] / self.dcurrency[self.base]
 
     def selling_price(self, nominal, curr):
         pass
         # Mulai kode anda dari sini
-        curr2idr = nominal * self.dcurrency[curr]
-        idr2sell = curr2idr / self.dcurrency[self.base]
-        sellPercent = (idr2sell * self.percent) / 100
-        sellCurr = idr2sell + sellPercent
+        c2s = nominal * self.dcurrency[curr]
+        sellPercent = (c2s * self.percent) / 100
+        sellCurr = c2s + sellPercent
         return sellCurr
 
     def buying_price(self, nominal, curr):
         pass
         # Mulai kode anda dari sini
-        curr2idr = nominal * self.dcurrency[curr]
-        idr2buy = curr2idr / self.dcurrency[self.base]
-        buyPercent = (idr2buy * self.percent) / 100
-        buyCurr = idr2buy - buyPercent
+        c2b = nominal * self.dcurrency[curr]
+        buyPercent = (c2b * self.percent) / 100
+        buyCurr = c2b - buyPercent
         return buyCurr
 
     def change_base(self, new_base):
         pass
         # Mulai kode anda dari sini
         self.base = new_base
+        for i in self.dcurrency:
+            if self.dcurrency[i] != self.dcurrency[self.base]:
+                self.dcurrency[i] = self.dcurrency[i] / self.dcurrency[self.base]
 
 # >>>>>>Akhir Soal 4
 
@@ -109,7 +113,8 @@ def main():
     #############################################
 
     print('test soal 1')
-    fungsiIO()  # contoh input: 7 4 5 2 1 3
+    list = (7,4,5,2,1,3)
+    fungsiIO(list)  # contoh input: 7 4 5 2 1 3
 
     print('test soal 2')
     print(caseShopia('thXGth876%^$LMn.'))
@@ -118,15 +123,17 @@ def main():
     print(usd2eur(100))  # mengubah 100 USD ke EUR
 
     print('test soal 4')
-    mc = MoneyChanger(dcur2idr, base='EUR')  # object mc dengan base EUR
+    mc = MoneyChanger(dcur2idr, base='SGD', percent=3)  # object mc dengan base EUR
     print('base', mc.base)
     print(mc.selling_price(100, 'USD'))  # nilai jual 100 USD ke EUR (Base)
     print(mc.buying_price(100, 'USD'))  # nilai beli 100 USD ke EUR (Base)
+    print(mc.dcurrency)
 
-    mc.change_base('IDR')  # Ganti Base object mc ke IDR
+    mc.change_base('JPY')  # Ganti Base object mc ke IDR
     print('base', mc.base)
-    print(mc.selling_price(100, 'USD'))  # nilai jual 100 USD ke IDR (Base)
-    print(mc.buying_price(100, 'USD'))  # nilai beli 100 USD ke IDR (Base)
+    print(mc.selling_price(100, 'GBP'))  # nilai jual 100 USD ke IDR (Base)
+    print(mc.buying_price(100, 'GBP'))  # nilai beli 100 USD ke IDR (Base)
+    print(mc.dcurrency)
 
     # >>>>>AKHIR TEST DI SINI>>>>>>
 
